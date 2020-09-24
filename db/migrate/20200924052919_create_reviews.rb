5d94c3facd1e046152312fe6279bcba4dd2d3057
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-24 15:12:05 +0900

    reviewモデルのマイグレーションファイルの修正

--- db/migrate/20200924052919_create_reviews.rb
+++ db/migrate/20200924052919_create_reviews.rb
@@ -4,7 +4,6 @@ class CreateReviews < ActiveRecord::Migration[6.0]
       t.content :text ,null: false
       t.user :references, null: false, foreign_key: true
       t.beer :references, null: false, foreign_key: true
-      t.tag :references
       t.timestamps
     end
   end
