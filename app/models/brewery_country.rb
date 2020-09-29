0f09af40d3e3a103f200ece0c13956860431dcca
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-29 11:22:37 +0900

    activehashの導入とモデルの作成

---
+++ app/models/brewery_country.rb
@@ -0,0 +1,2 @@
+class BreweryCountry < ApplicationRecord
+end
---
+++ test/models/brewery_country_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BreweryCountryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
