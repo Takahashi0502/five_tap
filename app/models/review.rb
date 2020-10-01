808d4f904920b7389cc67568826e40171d7cab0b
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-01 11:20:55 +0900

    タグ付け機能のためのリレーションを変更

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :beer
   belongs_to :user
-  has_many :review_tags
+  has_many :review_tags, dependent: :destroy
   has_many :tags, through: :review_tags
   has_many :likes
   has_many :users, through: :likes
