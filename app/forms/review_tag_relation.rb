88d993b7c09d9f873aca55dc0515472026e9fbeb
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-01 15:43:05 +0900

    複数のタグを入力できるように修正

--- app/forms/review_tag_relation.rb
+++ app/forms/review_tag_relation.rb
@@ -4,9 +4,13 @@ class ReviewTagRelation
 
   def save
     review = Review.create(content: content, images: images, beer_id: beer_id, user_id: user_id)
-    tag = Tag.where(name: name).first_or_initialize
-    tag.save
-
-    ReviewTag.create(review_id: review.id, tag_id: tag.id)
+    # split(/[[:blank:]]+/)でスペース区切りで配列にする
+    tag_list = name.split(/[[:blank:]]+/).select(&:present?)
+    # 複数のタグが入力された時に、タグ毎に保存させる
+    tag_list.each do |t|
+      tag = Tag.where(name: t).first_or_initialize
+      tag.save
+      ReviewTag.create(review_id: review.id, tag_id: tag.id)
+    end
   end
 end
