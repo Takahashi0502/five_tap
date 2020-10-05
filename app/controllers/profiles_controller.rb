fb9cc1b185f4f9ea58292b03146b93c693130773
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-05 18:22:08 +0900

    profilesコントローラの作成

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,19 @@
+class ProfilesController < ApplicationController
+
+  def edit
+    @profile = Profile.find_by(user_id: params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+    binding.pry
+    @profile.update(profile_edit_params)
+    redirect_to user_path(@profile.user_id)
+  end
+
+  private
+
+  def profile_edit_params
+    params.require(:profile).permit(:avatar, :favorite_beer, :twitter_link, :info).merge(user_id: current_user.id)
+  end
+end
