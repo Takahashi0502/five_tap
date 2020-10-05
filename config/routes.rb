f6aba04800a188a7c6afbfda852e6795e252dd02
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-05 20:17:32 +0900

    日本語表記を修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'profiles', to: 'users/registrations#new_profile'
     post 'profiles', to: 'users/registrations#create_profile'
-
   end
   resources :users, only: [:show] do
     resources :relationships, only: [:create, :destroy]
