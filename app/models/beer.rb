c5944b77274df2dd6986c48a7a3b7865b152bb8d
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-06 13:02:26 +0900

    beerモデルのバリデーションを記載

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -9,6 +9,8 @@ class Beer < ApplicationRecord
   belongs_to_active_hash :style
   belongs_to_active_hash :brewery_country
 
+  validates :name, presence: true
+  validates :url, format: { with: /\A#{URI::regexp(%w(http https))}\z/, message: "URLを入力してください" }
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
