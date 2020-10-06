aa83a009774cd0de9375be45443e40abcdf24b19
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-06 14:40:14 +0900

    reviewモデルのバリデーションを記載

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,6 +7,8 @@ class Review < ApplicationRecord
   has_many :users, through: :likes
   has_many_attached :images
 
+  validates :content, presence: true
+
   def liked_by?(user)
     likes.where(user_id: user.id).exists?
   end
