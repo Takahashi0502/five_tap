1080d46c40a61428aff661de1fe4540d98519b68
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-06 13:01:12 +0900

    profileモデルのバリデーションを記載

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
   has_one_attached :avatar
+
+  validates :twitter_link, format: { with: /\A#{URI::regexp(%w(http https))}\z/, message: "URLを入力してください" }
 end
