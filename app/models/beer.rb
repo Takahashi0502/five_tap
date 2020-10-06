c248d0f5a775a2530f742ed5bfa39e83ec12316e
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-06 15:28:09 +0900

    beerモデルのエラーメッセージを修正

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -10,7 +10,7 @@ class Beer < ApplicationRecord
   belongs_to_active_hash :brewery_country
 
   validates :name, presence: true
-  validates :url, format: { with: /\A#{URI::regexp(%w(http https))}\z/, message: "URLを入力してください" }
+  validates :url, format: { with: /\A#{URI::regexp(%w(http https))}\z/ , message: "はURL形式で入力してください" }, allow_blank: true
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
