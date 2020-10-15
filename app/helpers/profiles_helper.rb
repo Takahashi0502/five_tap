d62ab4275d9edee2fe1a62150aaeb831436bd06e
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-15 10:55:24 +0900

    ユーザーアイコンの表示をヘルパーメソッド化

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,10 @@
 module ProfilesHelper
+
+  def show_avatar(user)
+    if user.profile.avatar.attached?
+      image_tag user.profile.avatar.variant(resize:'40x40'), {class: "user-icon"}
+    else
+      image_tag "default_user", {class: "user-icon", size: "40x40"}
+    end
+  end
 end
