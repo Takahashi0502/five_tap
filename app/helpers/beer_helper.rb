0e545e0a265ebec7119f2d65d258704870317e35
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-16 19:10:03 +0900

    ビールの画像表示をヘルパーメソッド化

--- app/helpers/beer_helper.rb
+++ app/helpers/beer_helper.rb
@@ -1,2 +1,10 @@
 module BeerHelper
+
+  def show_beerimage(beer)
+    if beer.beer_image.attached?
+      image_tag beer.beer_image.variant(resize:'300x300'), {class: "beer-icon"}
+    else
+      image_tag "default_beer", {class: "beer-icon", size: "300x300"}
+    end
+  end
 end
