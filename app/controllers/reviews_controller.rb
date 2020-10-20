3b7544f91b28089bfc7d5fe32d1a1ce289a1e206
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-20 15:21:58 +0900

    コントローラーの修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :move_to_signin, only: [:new, :create]
-  
+  before_action :set_beer, only: [:new, :create]
+
   def new
     @review = ReviewTagRelation.new
   end
@@ -10,9 +11,9 @@ class ReviewsController < ApplicationController
     @review.user_id = current_user.id
     if @review.valid?
       @review.save
-      return redirect_to beer_path(params[:beer_id])
+      return redirect_to beer_path(@beer.id)
     else
-      @reviews = Review.where(beer_id: params[:beer_id])
+      @reviews = Review.where(beer_id: @beer.id)
       render 'reviews/new'
     end
   end
@@ -26,5 +27,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review_tag_relation).permit(:content, :images, :name).merge(beer_id: params[:beer_id])
+  end
+
+  def set_beer
+    @beer = Beer.find(params[:beer_id])
   end
 end
