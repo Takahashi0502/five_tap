394a665e51810e3c889541fa88c828bcc3553cac
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-21 18:45:30 +0900

    画像のアセットパイプラインを変更

--- app/helpers/beer_helper.rb
+++ app/helpers/beer_helper.rb
@@ -4,7 +4,7 @@ module BeerHelper
     if beer.beer_image.attached?
       image_tag beer.beer_image.variant(resize:'300x300'), {class: "beer-icon"}
     else
-      image_tag "default_beer", {class: "beer-icon", size: "300x300"}
+      image_tag asset_path('default_beer.png'), {class: "beer-icon", size: "300x300"}
     end
   end
 end
