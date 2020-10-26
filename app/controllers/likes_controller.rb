6b834895ad049580f6a0c641baf28fdaf5b76727
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-26 15:36:31 +0900

    renderで渡すパラメーターの値を修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
   def create
+    @review = Review.find(params[:review_id])
     like = current_user.likes.build(review_id: params[:review_id])
     like.save
   end
 
   def destroy
+    @review = Review.find(params[:review_id])
     like = Like.find_by(review_id: params[:review_id], user_id: current_user.id)
     like.destroy
   end
