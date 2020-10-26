2a7e9987848997620ad1948d8058b2c8e90a10aa
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-26 14:45:08 +0900

    ビア活のいいね機能をajax通信に記述を変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.build(review_id: params[:review_id])
     like.save
-    redirect_to review_path(params[:review_id])
   end
 
   def destroy
     like = Like.find_by(review_id: params[:review_id], user_id: current_user.id)
     like.destroy
-    redirect_to review_path(params[:review_id])
   end
 end
