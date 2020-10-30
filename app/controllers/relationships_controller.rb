54961a8a70fc60d398f8cba92dae6e5f21ca2eeb
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-10-30 13:58:56 +0900

    相互フォローの記述をリファクタリング

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,13 @@
 class RelationshipsController < ApplicationController
   def create
-    follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    follow.save
+    user = User.find(params[:user_id])
+    current_user.follow(user)
     redirect_to user_path(params[:user_id])
   end
 
   def destroy
-    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
-    follow.destroy
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
     redirect_to user_path(params[:user_id])
   end
 end
