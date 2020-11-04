6bebb10c414a9e800ff6918989e22e9fa7a1c3bb
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-11-04 15:52:50 +0900

    reviewTagRelationモデルのテストコードを修正

--- spec/models/review_tag_relation_spec.rb
+++ spec/models/review_tag_relation_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ReviewTagRelation, type: :model do
   describe 'ビア活の新規登録' do
+    before do
+      @review = FactoryBot.build(:review_tag_relation)
+    end
     it 'contentが入力されていれば登録できること' do
-      review = ReviewTagRelation.new( content: 'aaaaaa' )
-      expect(review).to be_valid
+      @review.content = 'aaaaaa'
+      expect(@review).to be_valid
     end
 
     it 'contentが空だと登録できないこと'do
-      review = ReviewTagRelation.new( content: '' )
-      review.valid?
-      expect(review.errors.full_messages).to include("Content can't be blank")
+      @review.content = ''
+      @review.valid?
+      expect(@review.errors.full_messages).to include("Content can't be blank")
     end
   end
 end
