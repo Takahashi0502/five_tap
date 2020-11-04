6c0e56a302f5a5798ff905b5a3cb35bd6584526f
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-11-04 16:15:03 +0900

    factoryBotの修正

--- spec/factories/beers.rb
+++ spec/factories/beers.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :beer do
     name { Faker::Beer.name }
+    category_id { 1 }
+    style_id { 1 }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.first_name }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { 'abc123' }
     password_confirmation { password }
   end
 end
